e544505706c9917a304347fa7913323f34803deb
Alastair Brunton <info@simplyexcited.co.uk>
2011-09-11 21:21:04 +0200

    Moving forward with the config and development of the cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,28 @@
 default[:backup][:backup_user] = 'deploy'
-default[:backup][:config][:name] = "server_backup"
-default[:backup][:config][:description] = "a chef generated server backup"
 
-default[:backup][:config][:s3][:enabled] = true
-default[:backup][:config][:s3sync][:enabled] = true
+default[:backup][:name] = "server_backup"
+default[:backup][:description] = "a chef generated server backup"
 
-# need to add more default config here.
+
+default[:backup][:database][:name]
+default[:backup][:database][:user]
+default[:backup][:database][:password]
+
+
+default[:backup][:s3][:aws_access_key]
+default[:backup][:s3][:aws_secret_key]
+default[:backup][:s3][:bucket_region]
+default[:backup][:s3][:bucket_name]
+default[:backup][:s3][:keep]
+
+default[:backup][:database][:databases]
+
+default[:backup][:s3][:sync_path] 
+
+default[:backup][:s3][:sync_directories]
+
+default[:backup][:mail][:on_success]
+default[:backup][:mail][:on_failure]
+default[:backup][:mail][:from_address]
+default[:backup][:mail][:to_address]
+default[:backup][:mail][:domain]
